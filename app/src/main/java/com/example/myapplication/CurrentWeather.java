73e39da0b08f57a84b476a240c3023b7278cea03
Gelmis Bartulis <gelmisb@gmail.com>
2019-06-14 17:32:20 +0100

    ./gradlew is producing errors for TravisCi

--- app/src/main/java/com/example/myapplication/CurrentWeather.java
+++ app/src/main/java/com/example/myapplication/CurrentWeather.java
@@ -3,8 +3,19 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CurrentWeather extends AppCompatActivity {
+
+    // @TODO In this Activity it should display the current weather of the current location
+    // When the user will try to see another city,
+    // they can type the city name and add it,
+    // the application will then send the user to the 16 day
+    // forecast
+
+
+    private TextView cityNameText, timeText, dateText, phraseText, currentTempText, minTempText, maxTempText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
