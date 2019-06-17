f58e6ef9a7683012a8144db3553308971cab2899
Gelmis Bartulis <gelmisb@gmail.com>
2019-06-17 11:55:52 +0100

    Time and date addition - see LoadList class

--- app/src/main/java/com/example/myapplication/Controllers/LoadList.java
+++ app/src/main/java/com/example/myapplication/Controllers/LoadList.java
@@ -3,6 +3,7 @@ package com.example.myapplication.Controllers;
 import android.app.Activity;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +14,18 @@ import android.widget.TextView;
 import com.example.myapplication.R;
 import com.squareup.picasso.Picasso;
 
+import java.text.SimpleDateFormat;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.format.TextStyle;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import javax.crypto.spec.DESedeKeySpec;
+
+import static android.content.ContentValues.TAG;
 
 
 public class LoadList extends ArrayAdapter<String> {
@@ -68,6 +78,7 @@ public class LoadList extends ArrayAdapter<String> {
         TextView minTempText =  rowView.findViewById(R.id.minTemp);
         TextView maxTempText =  rowView.findViewById(R.id.maxTemp);
         TextView descriptionText =  rowView.findViewById(R.id.description);
+        TextView weekDay =  rowView.findViewById(R.id.weekDay);
         ImageView imageView = rowView.findViewById(R.id.profile);
 
         Typeface face = Typeface.createFromAsset(context.getAssets(),"fonts/Raleway-Regular.ttf");
@@ -75,22 +86,30 @@ public class LoadList extends ArrayAdapter<String> {
         phraseText.setTypeface(face);
         minTempText.setTypeface(face);
         maxTempText.setTypeface(face);
+        weekDay.setTypeface(face);
         descriptionText.setTypeface(face);
 
-        degreesText.setShadowLayer(2, 5, 5, Color.BLACK);
-        phraseText.setShadowLayer(2, 5, 5, Color.BLACK);
-        minTempText.setShadowLayer(2, 5, 5, Color.BLACK);
-        maxTempText.setShadowLayer(2, 5, 5, Color.BLACK);
-        descriptionText.setShadowLayer(2, 5, 5, Color.BLACK);
+        degreesText.setShadowLayer(3, 5, 5, Color.BLACK);
+        phraseText.setShadowLayer(2, 3, 3, Color.BLACK);
+        minTempText.setShadowLayer(2, 3, 3, Color.BLACK);
+        maxTempText.setShadowLayer(2, 3, 3, Color.BLACK);
+        descriptionText.setShadowLayer(2, 3, 3, Color.BLACK);
+        weekDay.setShadowLayer(2, 3, 3, Color.BLACK);
 
         // Making sure the main information is there
         if(degrees.get(position) != null){
             degreesText.setText(degrees.get(position) + "°C");
             phraseText.setText(mPhrase.get(position));
-            minTempText.setText(minTemp.get(position) + "°C");
-            maxTempText.setText(maxTemp.get(position) + "°C");
+            minTempText.setText("L: " + minTemp.get(position) + "°C");
+            maxTempText.setText("H: " + maxTemp.get(position) + "°C");
             descriptionText.setText(desc.get(position));
 
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE dd-MMM-yyyy");
+            Calendar calendar = new GregorianCalendar();
+            calendar.add(Calendar.DATE, position);
+            String day = sdf.format(calendar.getTime());
+            weekDay.setText(day);
+            
             // Used Picasso Library to download images, refactor images and to add them to imageViews
             Picasso.get()
                     .load("https://openweathermap.org/img/w/" + imageArray.get(position) + ".png")
